94643a0257ca5f0baf58666ec758c513deba74ce
sjcostan <shirwyn@pearlpay.io>
2021-03-04 16:09:43 +0800

    change propTypes

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -11,8 +11,8 @@ export default class Helper extends PureComponent {
 
     disabled: PropTypes.bool,
 
-    style: Text.propType,
-  
+    style: Text.propTypes.style,
+
     baseColor: PropTypes.string,
     errorColor: PropTypes.string,
 
--- src/components/label/index.js
+++ src/components/label/index.js
@@ -44,7 +44,7 @@ export default class Label extends PureComponent {
       y1: PropTypes.number,
     }),
 
-    style: Text.propType,
+    style: Text.propTypes.style,
     label: PropTypes.string,
   };
 
